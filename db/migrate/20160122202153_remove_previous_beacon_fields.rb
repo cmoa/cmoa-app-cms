e40136f9b9c24b7c26ae895f48c79066816098ab
Drew McDermott <code@drewmcdermott.net>
2016-01-22 15:45:30 -0500

    Setting up database for beacon table

--- db/migrate/20160122202153_remove_previous_beacon_fields.rb
+++ db/migrate/20160122202153_remove_previous_beacon_fields.rb
@@ -1,4 +1,8 @@
 class RemovePreviousBeaconFields < ActiveRecord::Migration
   def change
+    remove_column :locations, :beacon_major
+    remove_column :locations, :beacon_minor
+    remove_column :artworks, :beacon_major
+    remove_column :artworks, :beacon_minor
   end
 end
--- db/migrate/20160122202210_add_beacon_table.rb
+++ db/migrate/20160122202210_add_beacon_table.rb
@@ -1,4 +1,13 @@
 class AddBeaconTable < ActiveRecord::Migration
   def change
+    create_table :beacons do |t|
+      t.integer :major, null: false
+      t.integer :minor, null: false
+      t.string :beacon_ident, null: false
+    end
+    add_index :beacons, :beacon_ident, :unique => true
+
+    add_reference :artworks, :beacons, index: true
+    add_reference :locations, :beacons, index: true
   end
 end
