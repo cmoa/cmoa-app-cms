c50d10193f28f2e127b164fcafd3dd3a0f3b6f81
Drew McDermott <code@drewmcdermott.net>
2016-01-22 14:31:34 -0500

    Told the migration what to do

--- db/migrate/20160122191544_add_beacon_i_ds_to_locations.rb
+++ db/migrate/20160122191544_add_beacon_i_ds_to_locations.rb
@@ -1,4 +1,8 @@
 class AddBeaconIDsToLocations < ActiveRecord::Migration
-  def change
-  end
+
+    def change
+      add_column :locations, :beacon_major, :integer
+      add_column :locations, :beacon_minor, :integer
+    end
+
 end
