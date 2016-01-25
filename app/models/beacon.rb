e4c7d880e4790943b5b05dc70fe6e39c05743727
drew <mcdermotta@carnegiemuseums.org>
2016-01-25 15:57:13 -0500

    Beacon validation

--- app/models/beacon.rb
+++ app/models/beacon.rb
@@ -1,3 +1,5 @@
 class Beacon < ActiveRecord::Base
-
+  validates_uniqueness_of :major, :scope => :minor
+  validates_inclusion_of :major, :in => 1..65535
+  validates_inclusion_of :minor, :in => 1..65535
 end
