cac5a7ecf0137a06a000f4ca1522006f1ce5aac6
drew <mcdermotta@carnegiemuseums.org>
2016-01-25 15:51:54 -0500

    Create a beacon controller edits

--- app/controllers/beacons_controller.rb
+++ app/controllers/beacons_controller.rb
@@ -11,8 +11,13 @@ class BeaconsController < ApplicationController
   end
 
   def create
-    @beacon = Beacon.new
-    render action: 'new'
+    @beacon = Beacon.new(beacon_params)
+
+    if @beacon.save
+      redirect_to @beacon, notice: 'Beacon was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
   def edit
@@ -22,5 +27,9 @@ class BeaconsController < ApplicationController
   end
 
   def delete
+  end
+
+  def beacon_params
+      params.require(:beacon).premit(:major, :minor)
   end
 end
