d1e6838b5aca41a3d0ac4f2e5e4be19fb56cf800
drew <mcdermotta@carnegiemuseums.org>
2016-01-25 13:40:43 -0500

    Changing beacon uniqueness requirements

--- db/migrate/20160125161615_alter_beacon_unique_keys.rb
+++ db/migrate/20160125161615_alter_beacon_unique_keys.rb
@@ -1,4 +1,7 @@
 class AlterBeaconUniqueKeys < ActiveRecord::Migration
   def change
+    remove_column :beacons, :beacon_ident
+
+    add_index :beacons, [:major, :minor], :unique => true
   end
 end
