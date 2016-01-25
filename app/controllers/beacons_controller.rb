524761f741d6be9f5a0b4faf36e13714d8107dc2
drew <mcdermotta@carnegiemuseums.org>
2016-01-25 15:16:30 -0500

    Beacons index page & routing

--- app/controllers/beacons_controller.rb
+++ app/controllers/beacons_controller.rb
@@ -1,2 +1,23 @@
 class BeaconsController < ApplicationController
+  def index
+    @beacons = Beacon.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
 end
