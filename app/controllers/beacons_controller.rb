862b429425db0420914ebf1569a47e38647ca2b5
Drew McDermott <code@drewmcdermott.net>
2016-02-01 09:19:07 -0500

    Changing params for beacons

--- app/controllers/beacons_controller.rb
+++ app/controllers/beacons_controller.rb
@@ -30,6 +30,6 @@ class BeaconsController < ApplicationController
   end
 
   def beacon_params
-      params.require(:beacon).require(:name, :major, :minor)
+      params.require(:beacon).require(:major).require(:minor).permit(:name)
   end
 end
