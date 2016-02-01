d7cc8c45ef2882d3a1e7d12c7087ae3755acdd31
Drew McDermott <code@drewmcdermott.net>
2016-02-01 09:32:31 -0500

    Added details to beacon 'show'

--- app/models/beacon.rb
+++ app/models/beacon.rb
@@ -1,8 +1,10 @@
 class Beacon < ActiveRecord::Base
   validates_uniqueness_of :major, :scope => :minor
+  validates_uniqueness_of :name
   validates_inclusion_of :major, :in => 1..65535
   validates_inclusion_of :minor, :in => 1..65535
 
   validates :major, :presence => true
   validates :minor, :presence => true
+  validates :name, :presence => true
 end
