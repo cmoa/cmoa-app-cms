ce1e51e93e1ac93d5446a51564b993e115cbd746
Drew McDermott <code@drewmcdermott.net>
2016-02-01 10:01:27 -0500

    Added helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_admin!
+  helper_method :exhibition_is_set
 
   def set_exhibition
     @exhibition = Exhibition.find(params[:exhibition_id])
