807a834900c9a34620dd94ad54c77917c1d0c267
Drew McDermott <code@drewmcdermott.net>
2016-02-02 16:03:18 -0500

    remove @ sign

--- app/models/beacon.rb
+++ app/models/beacon.rb
@@ -16,7 +16,7 @@ class Beacon < ActiveRecord::Base
 
   JSON_ATTRS = ["major", "minor", "name"]
 
-  def self.unassigned(@selected)
+  def self.unassigned(selected)
     return Beacon.where('(id NOT IN ((SELECT DISTINCT(beacon_id) FROM artworks) UNION (SELECT DISTINCT(beacon_id) FROM locations))) OR (id = #{@selected})')
   end
 
