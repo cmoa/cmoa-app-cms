03f1a20ed29166545f1ac8ae9284582a991b627c
Drew McDermott <code@drewmcdermott.net>
2016-02-02 16:05:21 -0500

    Change unassigned query

--- app/models/beacon.rb
+++ app/models/beacon.rb
@@ -17,7 +17,7 @@ class Beacon < ActiveRecord::Base
   JSON_ATTRS = ["major", "minor", "name"]
 
   def self.unassigned(selected)
-    return Beacon.where('(id NOT IN ((SELECT DISTINCT(beacon_id) FROM artworks) UNION (SELECT DISTINCT(beacon_id) FROM locations))) OR (id = #{@selected})')
+    return Beacon.where("(id NOT IN ((SELECT DISTINCT(beacon_id) FROM artworks) UNION (SELECT DISTINCT(beacon_id) FROM locations))) OR (id = '#{@selected}')")
   end
 
 
