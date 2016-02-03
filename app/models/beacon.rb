8870ee96bace55d6aaa61488f5e82b8fdcb9d780
drew <mcdermotta@carnegiemuseums.org>
2016-02-03 14:34:56 -0500

    Fixing some associations

--- app/models/beacon.rb
+++ app/models/beacon.rb
@@ -1,5 +1,5 @@
 class Beacon < ActiveRecord::Base
-  has_one :artwork
+  has_one :artwork, :dependent => :nullify
   has_one :location
 
 
@@ -14,7 +14,7 @@ class Beacon < ActiveRecord::Base
   validates :minor, :presence => true
   validates :name, :presence => true
 
-  JSON_ATTRS = ["major", "minor", "name"]
+  JSON_ATTRS = ["id", "major", "minor", "name"]
 
 
   def self.unassigned(selected)
