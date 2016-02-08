039f93aa1a66edd11dfcff7810fc00768e4f2ace
drew <mcdermotta@carnegiemuseums.org>
2016-02-08 16:04:12 -0500

    Fixing some more category paths

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,7 +21,7 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to [@exhibition, @category], notice: 'Category was successfully created.'
+      redirect_to @category, notice: 'Category was successfully created.'
     else
       render action: 'new'
     end
@@ -29,7 +29,7 @@ class CategoriesController < ApplicationController
 
   def update
     if @category.update(category_params)
-      redirect_to [@exhibition, @category], notice: 'Category was successfully updated.'
+      redirect_to @category, notice: 'Category was successfully updated.'
     else
       render action: 'edit'
     end
