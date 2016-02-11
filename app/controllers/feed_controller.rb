fc08c37c2849987313eb1b133e2e8e98dd525422
drew <mcdermotta@carnegiemuseums.org>
2016-02-11 11:16:50 -0500

    Feed form and prereqs

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,3 +1,15 @@
 class FeedsController < ApplicationController
 
+  def index
+    @feeds = Feed.all
+  end
+
+  def new
+    @feed = Feed.new
+  end
+
+
+  def feed_params
+    params.require(:feed).permit(:name, :url, :type)
+  end
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,9 @@
 class Feed < ActiveRecord::Base
+    validates :name, :presence => true
+    validates :url, :presence => true
+    validates :type, :presence => true
+
+    enum type: [:news, :video]
+
 
 end
