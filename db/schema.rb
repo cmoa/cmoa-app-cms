2cb74b2bc918fdebaa5de61972f15c816928146d
Ruben Niculcea <ruben.niculcea@gmail.com>
2016-02-11 11:43:23 -0500

    Migrate complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211152421) do
+ActiveRecord::Schema.define(version: 20160211164009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,7 +114,7 @@ ActiveRecord::Schema.define(version: 20160211152421) do
   create_table "feeds", force: true do |t|
     t.string   "name"
     t.string   "url"
-    t.integer  "type"
+    t.integer  "feed_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
