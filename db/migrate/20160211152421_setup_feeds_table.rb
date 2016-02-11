4ee50c1aeb18bb12be8022faa4666e5287572762
drew <mcdermotta@carnegiemuseums.org>
2016-02-11 10:30:38 -0500

    Setup table

--- db/migrate/20160211152421_setup_feeds_table.rb
+++ db/migrate/20160211152421_setup_feeds_table.rb
@@ -1,4 +1,9 @@
 class SetupFeedsTable < ActiveRecord::Migration
   def change
+    create_table :feeds do |t|
+      t.string    :name
+      t.string    :url
+      t.integer   :type
+      t.timestamps
   end
 end
