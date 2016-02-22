199c9de6d0d5b912982ed9f79b62dd8bd9a1a962
drew <mcdermotta@carnegiemuseums.org>
2016-02-22 10:54:55 -0500

    Set password on creation

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -9,19 +9,23 @@ class AdminsController < ApplicationController
   end
 
   def create
-    pass = Devise.friendly_token.first(8)
+    pass1 = params[:admin][:password]
+    pass2 = params[:admin][:password_again]
     email = params[:admin][:email]
 
-    @admin = Admin.new
-    @admin.email = email
-    @admin.password = pass
+    if pass1.to_s == pass2.to_s #If the passwords match
 
-    if @admin.save
-      #RegistrationMailer.welcome(@admin, pass).deliver
-      UserMailer.new_admin(@admin, pass).deliver
-      redirect_to action: "index", notice: "An Admin was created.  A password was sent to them."
-    else
-      render action: 'new'
+      @admin = Admin.new
+      @admin.email = email
+      @admin.password = pass1.to_s
+
+      if @admin.save
+        #UserMailer.new_admin(@admin, pass).deliver
+        redirect_to action: "index", notice: "An Admin was created."
+      else
+        render action: 'new'
+      end
+
     end
 
   end
