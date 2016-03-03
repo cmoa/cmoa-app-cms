7c10b9d91dc2749236636e2c2cdf5308c8456fb5
drew <mcdermotta@carnegiemuseums.org>
2016-03-03 13:40:51 -0500

    Setup migration

--- db/migrate/20160303182828_change_beacons_for_multi_location.rb
+++ db/migrate/20160303182828_change_beacons_for_multi_location.rb
@@ -1,4 +1,16 @@
 class ChangeBeaconsForMultiLocation < ActiveRecord::Migration
   def change
+    #remove old indexes
+    remove_column :artwork, :beacon_id, :integer
+    remove_column :location, :beacon_id, :integer
+
+    #add new indexes
+    add_index :beacons, :artwork
+    add_index :beacons, :locations
+
+    #add timestamps
+    add_column(:beacons, :created_at, :datetime)
+    add_column(:beacons, :updated_at, :datetime)
+
   end
 end
